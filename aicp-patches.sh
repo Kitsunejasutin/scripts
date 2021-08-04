37ee3511cd3251547ff4c926b7812c2915244039
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-08-04 23:37:37 +0800

    acip-patches: Increase Java heap space for metalava * cause I build on 8GB RAM

--- aicp-patches.sh
+++ aicp-patches.sh
@@ -10,4 +10,8 @@ git cherry-pick d5bf33497b07938574a593549324452cc32c9c04
 # libcameraservice: Add support for miui camera mode
 git fetch https://github.com/Evolution-X/frameworks_av
 git cherry-pick fc59677b7dbecc56187e30a47959a2b7898c28c5
-
+#
+# soong: java: Specify larger heap size for metalava
+cd ../../build/soong
+git fetch "https://github.com/LineageOS/android_build_soong" refs/changes/26/289926/2 && git cherry-pick FETCH_HEAD
+# 
